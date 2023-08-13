44c71a86c3a01407fe3ed618cc4c2eb8bffb8fad
vigneshSr91 <vignesh.sr.1991@gmail.com>
2023-08-13 22:41:59 +0530

    splash

---
+++ lib/splash.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  _SplashState createState() => _SplashState();
+
+}
+class _SplashState extends State<Splash>{
+  @override
+  void initState(){
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome()async{
+    await Future.delayed(const Duration(milliseconds: 1500),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title: 'Sakthi Amma',)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: const Text(
+            'Splash Screen',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+        ),
+      ),
+    );
+  }
+}
